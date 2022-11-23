13d11d6b459e99fa82ead839d0dfaef6f54c1477
Ciprian <cursu@axetrading.com>
2022-11-23 12:58:57 +0200

    fixing typo

--- iam.tf
+++ iam.tf
@@ -45,7 +45,7 @@ resource "aws_iam_role" "this" {
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
-  for_each = [for policies in toset(var.role_policy_arns) : policies if var.create_role && length(var.role_policy_arn) > 0]
+  for_each = [for policies in toset(var.role_policy_arns) : policies if var.create_role && length(var.role_policy_arns) > 0]
 
   role       = aws_iam_role.this[0].name
   policy_arn = each.value
