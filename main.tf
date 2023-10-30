9fdd1e4b0798b499be0d3799d2a5aa00306e7974
Vivek Singh <vivek.singh@tradingtechnologies.com>
2023-10-30 17:22:28 +0530

    liveness probe for statefuleset exec cmd

--- main.tf
+++ main.tf
@@ -116,9 +116,9 @@ resource "helm_release" "main" {
     value = "{${join(",", var.container_commands_args)}}"
   }
 
-  set_list {
+  set {
     name  = "healthcheck.execCommands"
-    value = var.health_check_exec_commands
+    value = jsonencode(var.health_check_exec_commands)
   }
 
   set {
